5ab0df13f0ef85ba5ee63ebfc7af0fcbd0a7db69
kiet24032003 <nguyenquangkiet123aa@gmail.com>
2025-02-23 14:22:26 +0700

    giao dien admin

--- datn_wd30/src/admin/HomeAdmin.tsx
+++ datn_wd30/src/admin/HomeAdmin.tsx
@@ -36,36 +36,6 @@ const HomeAdmin = () => {
                           <span>Quản lý sản phẩm</span>
                       </Link>
                   </li>
-                  <li  className="sub-menu">
-                      <a href="javascript:;" >
-                          <i  className="fa fa-cogs"></i>
-                          <span>Components</span>
-                      </a>
-                      <ul  className="sub">
-                          <li><a  href="grids.html">Grids</a></li>
-                          <li><a  href="calendar.html">Calendar</a></li>
-                          <li><a  href="gallery.html">Gallery</a></li>
-                          <li><a  href="todo_list.html">Todo List</a></li>
-                          <li><a  href="draggable_portlet.html">Draggable Portlet</a></li>
-                          <li><a  href="tree.html">Tree View</a></li>
-                      </ul>
-                  </li>
-                  <li  className="sub-menu">
-                      <a href="javascript:;" >
-                          <i  className="fa fa-tasks"></i>
-                          <span>Form Stuff</span>
-                      </a>
-                      <ul  className="sub">
-                          <li><a  href="form_component.html">Form Components</a></li>
-                          <li><a  href="advanced_form_components.html">Advanced Components</a></li>
-                          <li><a  href="form_wizard.html">Form Wizard</a></li>
-                          <li><a  href="form_validation.html">Form Validation</a></li>
-                          <li><a  href="dropzone.html">Dropzone File Upload</a></li>
-                          <li><a  href="inline_editor.html">Inline Editor</a></li>
-                          <li><a  href="image_cropping.html">Image Cropping</a></li>
-                          <li><a  href="file_upload.html">Multiple File Upload</a></li>
-                      </ul>
-                  </li>
                   <li>
                       <Link  className="active" to={'size'}>
                       <i  className="fa fa-laptop"></i>
@@ -108,38 +78,9 @@ const HomeAdmin = () => {
                           <span>Quản lý màu sắc</span>
                       </Link>
                   </li>
-                  <li>
-                      <a  href="login.html">
-                          <i  className="fa fa-user"></i>
-                          <span>Login Page</span>
-                      </a>
-                  </li>
-                  <li  className="sub-menu">
-                      <a href="javascript:;" >
-                          <i  className="fa fa-sitemap"></i>
-                          <span>Multi level Menu</span>
-                      </a>
-                      <ul  className="sub">
-                          <li><a  href="javascript:;">Menu Item 1</a></li>
-                          <li  className="sub-menu">
-                              <a  href="boxed_page.html">Menu Item 2</a>
-                              <ul  className="sub">
-                                  <li><a  href="javascript:;">Menu Item 2.1</a></li>
-                                  <li  className="sub-menu">
-                                      <a  href="javascript:;">Menu Item 3</a>
-                                      <ul  className="sub">
-                                          <li><a  href="javascript:;">Menu Item 3.1</a></li>
-                                          <li><a  href="javascript:;">Menu Item 3.2</a></li>
-                                      </ul>
-                                  </li>
-                              </ul>
-                          </li>
-                      </ul>
-                  </li>
-               
-
+                
               </ul>
-           
+              
           </div>
       </aside>
         <main  className='w-full h-full p-2'>
