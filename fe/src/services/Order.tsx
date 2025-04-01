2bca3d2d2592b12152b647184a1b1f5150eb5f26
nghiadvph <nghiadvph46437@fpt.edu.vn>
2025-04-01 23:55:05 +0700

    sửa checkbox sp sang checkout

--- fe/src/services/Order.tsx
+++ fe/src/services/Order.tsx
@@ -2,6 +2,12 @@ import { api } from "../config/axios"
 export const getCheckout = ()=>{
       return api.get("/checkout/init")
 }
+export const getCheckOut = (userId: number, selectedItems: number[]) => {
+      return api.post("/checkout/preview", {
+        item_ids: selectedItems, // Gửi đúng tên trường theo backend yêu cầu
+        user_id: userId, // Thêm user_id cho backend nhận diện người dùng
+      });
+    };
 export const getOrder = (orderData: any)=>{
       return api.post('/orders',orderData)
 }
